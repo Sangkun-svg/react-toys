1b8f5af02530054615dba256de4cf84b843dd2f8
Sangkun-svg <gims4752@gmail.com>
2022-10-17 20:47:52 +0900

    feat : generate date handler according of on change month

--- react-calendar/src/components/Header/Header.tsx
+++ react-calendar/src/components/Header/Header.tsx
@@ -1,3 +1,45 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import dayjs from "dayjs";
+
 export const Header = (props: any) => {
-  return <div>Header</div>;
+  const title = "CALENDAR";
+  const [currentMonth, setCurrentMonth] = useState(dayjs().month());
+  const [currentYear, setCurrentYear] = useState(dayjs().year());
+
+  useEffect(() => {
+    let dayOfMonth = dayjs(`${currentYear}-${currentMonth}`).daysInMonth();
+  }, [currentMonth, currentYear]);
+
+  const onNextMonth = () => {
+    const next = dayjs().set("M", currentMonth).add(1, "M").month();
+    if (next === 0) {
+      const nextYear = dayjs().set("y", currentYear).add(1, "y").year();
+      setCurrentYear(nextYear);
+    }
+    setCurrentMonth(next);
+  };
+  const onPrevMonth = () => {
+    const next = dayjs().set("M", currentMonth).subtract(1, "M").month();
+    if (next === 11) {
+      const nextYear = dayjs().set("y", currentYear).subtract(1, "y").year();
+      setCurrentYear(nextYear);
+    }
+    setCurrentMonth(next);
+  };
+
+  return (
+    <div>
+      <Title>{title}</Title>
+      <button onClick={onPrevMonth}>prev</button>
+      <h4>{currentYear}</h4>
+      <h1>{currentMonth + 1}</h1>
+      <button onClick={onNextMonth}>next</button>
+    </div>
+  );
 };
+
+const Title = styled.div`
+  color: gray;
+  font-weight: 500;
+`;
