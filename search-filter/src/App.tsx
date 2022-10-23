778db3f0f73d8dde6b605f7553e29b4bd468fba4
Sangkun-svg <gims4752@gmail.com>
2022-10-23 20:04:43 +0900

    search program rough ver

--- search-filter/src/App.tsx
+++ search-filter/src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { User } from "./mock/user";
+import _ from "lodash";
+import "./App.css";
+
+type user = {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  gender: string; // enum
+};
 
 function App() {
+  const [query, setQuery] = useState("");
+  const onQuery = ({ target: { value } }: any) => {
+    setQuery(value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={query}
+        placeholder="Search..."
+        className="search"
+        onChange={onQuery}
+      />
+      <ul className="list">
+        {User.filter((el) =>
+          el.first_name.toLocaleLowerCase().includes(query.toLowerCase())
+        ).map((el) => {
+          return (
+            <li key={el.id} className="listItem">
+              {el.first_name}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
