508d3624fec7b1badecd2ec8b44eff01871730ee
Sangkun-svg <gims4752@gmail.com>
2022-10-23 21:06:46 +0900

    generate User filter function & styling component using css-in-js library

--- search-filter/src/App.tsx
+++ search-filter/src/App.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import { User } from "./mock/user";
-import _ from "lodash";
+// import _ from "lodash"; TODO: usgin debounce
 import "./App.css";
+import styled from "styled-components";
+import DeleteIcon from "@mui/icons-material/Delete";
+
 import { Table } from "./Table";
 
 type user = {
@@ -14,43 +17,71 @@ type user = {
 
 function App() {
   const [query, setQuery] = useState("");
+  const [filterdUser, setFilterdUser] = useState(User);
+
+  useEffect(() => {
+    setFilterdUser(
+      User.filter(
+        (el: user) =>
+          el.first_name.toLocaleLowerCase().includes(query.toLowerCase()) ||
+          el.last_name.toLocaleLowerCase().includes(query.toLowerCase()) ||
+          el.email.toLocaleLowerCase().includes(query.toLowerCase())
+      )
+    );
+  }, [query]);
+
+  const deleteQuery = () => {
+    setQuery("");
+  };
+
   const onQuery = ({ target: { value } }: any) => {
     setQuery(value);
   };
+
   return (
     <div className="App">
-      <input
-        type="text"
-        value={query}
-        placeholder="Search..."
-        className="search"
-        onChange={onQuery}
-      />
-      <table>
-        <tbody>
-          <tr>
-            <th>Name</th>
-            <th>Surname</th>
-            <th>Email</th>
-          </tr>
-          {User.filter(
-            (el) =>
-              el.first_name.toLocaleLowerCase().includes(query.toLowerCase()) ||
-              el.last_name.toLocaleLowerCase().includes(query.toLowerCase()) ||
-              el.email.toLocaleLowerCase().includes(query.toLowerCase())
-          ).map((el) => {
-            return (
-              <tr key={el.id} className="listItem">
-                <td className="listContent">{el.first_name}</td>
-                <td className="listContent">{el.last_name}</td>
-                <td className="listContent">{el.email}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <Label>
+        <input
+          type="text"
+          placeholder="Search..."
+          className="search"
+          value={query}
+          onChange={onQuery}
+        />
+        <button onClick={deleteQuery}>
+          <DeleteIcon />
+        </button>
+      </Label>
+      {filterdUser.length === 0 && <h1>Not Found User</h1>}
+      {filterdUser.length >= 1 && <Table User={filterdUser} query={query} />}
     </div>
   );
 }
+const Label = styled.label`
+  position: relative;
 
+  input {
+    border: none;
+    padding: 0 15px;
+    height: 40px;
+    border: 2px solid blue;
+    border-radius: 15px;
+  }
+  button {
+    position: absolute;
+    background: none;
+    color: inherit;
+    border: none;
+    padding: 0;
+    font: inherit;
+    cursor: pointer;
+    outline: inherit;
+    top: 7px;
+    right: 10px;
+    :hover {
+      border: 1px solid black;
+      border-radius: 10px;
+    }
+  }
+`;
 export default App;
