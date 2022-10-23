23541e821dd5050426bba7242f9b4f8faac99618
Sangkun-svg <gims4752@gmail.com>
2022-10-23 20:35:54 +0900

    feat : add table

--- search-filter/src/App.tsx
+++ search-filter/src/App.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { User } from "./mock/user";
 import _ from "lodash";
 import "./App.css";
+import { Table } from "./Table";
 
 type user = {
   id: number;
@@ -25,17 +26,29 @@ function App() {
         className="search"
         onChange={onQuery}
       />
-      <ul className="list">
-        {User.filter((el) =>
-          el.first_name.toLocaleLowerCase().includes(query.toLowerCase())
-        ).map((el) => {
-          return (
-            <li key={el.id} className="listItem">
-              {el.first_name}
-            </li>
-          );
-        })}
-      </ul>
+      <table>
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>Email</th>
+          </tr>
+          {User.filter(
+            (el) =>
+              el.first_name.toLocaleLowerCase().includes(query.toLowerCase()) ||
+              el.last_name.toLocaleLowerCase().includes(query.toLowerCase()) ||
+              el.email.toLocaleLowerCase().includes(query.toLowerCase())
+          ).map((el) => {
+            return (
+              <tr key={el.id} className="listItem">
+                <td className="listContent">{el.first_name}</td>
+                <td className="listContent">{el.last_name}</td>
+                <td className="listContent">{el.email}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ search-filter/src/Table.tsx
@@ -0,0 +1,18 @@
+export const Table = ({ data }: any) => {
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th>Name</th>
+          <th>Surname</th>
+          <th>Email</th>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
