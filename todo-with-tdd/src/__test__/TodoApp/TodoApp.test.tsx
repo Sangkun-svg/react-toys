dce0cf65f6da74e9d84e5e65f82b3d9750481086
Sangkun-svg <gims4752@gmail.com>
2022-10-25 22:49:08 +0900

    test : render default value

--- todo-with-tdd/src/__test__/TodoApp/TodoApp.test.tsx
+++ todo-with-tdd/src/__test__/TodoApp/TodoApp.test.tsx
@@ -6,6 +6,12 @@ describe("<TodoApp />", () => {
   it("render TodoList and TodoForm", () => {
     render(<TodoApp />);
     screen.getByText("등록"); //  TodoForm 존재유무 확인
-    screen.getByTestId("TodoList");
+    screen.getByTestId("TodoList"); //  TodoList 존재유무 확인
+  });
+
+  it("render two default todos", () => {
+    render(<TodoApp />);
+    screen.getByText("TDD 배우기");
+    screen.getByText("react-testing-library 사용하기");
   });
 });
--- todo-with-tdd/src/components/TodoApp/TodoApp.tsx
+++ todo-with-tdd/src/components/TodoApp/TodoApp.tsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { TodoList, TodoForm } from "..";
 
 export const TodoApp = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "TDD 배우기",
+      done: true,
+    },
+    {
+      id: 2,
+      text: "react-testing-library 사용하기",
+      done: true,
+    },
+  ]);
   const onToggle = () => {};
   const onRemove = () => {};
   return (
     <React.Fragment>
       <TodoForm />
-      <TodoList todos={[]} onRemove={onRemove} onToggle={onToggle} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </React.Fragment>
   );
 };
