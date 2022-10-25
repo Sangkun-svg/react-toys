897794d34a4e0d958dffc9b8c3e97da4fdcb72d8
Sangkun-svg <gims4752@gmail.com>
2022-10-25 23:25:41 +0900

    feat : todo done status toggle function

--- todo-with-tdd/src/__test__/TodoApp/TodoApp.test.tsx
+++ todo-with-tdd/src/__test__/TodoApp/TodoApp.test.tsx
@@ -14,4 +14,23 @@ describe("<TodoApp />", () => {
     screen.getByText("TDD 배우기");
     screen.getByText("react-testing-library 사용하기");
   });
+
+  it("create new todo", () => {
+    render(<TodoApp />);
+    const input = screen.getByPlaceholderText("할 일을 입력해주세요");
+    const button = screen.getByText("등록");
+    fireEvent.change(input, { target: { value: "새 항목 추가하기" } });
+    fireEvent.click(button);
+    expect(screen.getByText("새 항목 추가하기")).toBeTruthy();
+  });
+
+  it("toggles todo", () => {
+    render(<TodoApp />); // default done : true
+    const todoItem = screen.getByText("TDD 배우기");
+    expect(todoItem).toHaveStyle("text-decoration: line-through;"); // done : true
+    fireEvent.click(todoItem); // toggle
+    expect(todoItem).toHaveStyle("text-decoration: none;"); // done : false
+    fireEvent.click(todoItem);
+    expect(todoItem).toHaveStyle("text-decoration: line-through;"); // done : true
+  });
 });
--- todo-with-tdd/src/components/TodoApp/TodoApp.tsx
+++ todo-with-tdd/src/components/TodoApp/TodoApp.tsx
@@ -1,8 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useCallback } from "react";
 import { TodoList, TodoForm } from "..";
 
+type Todo = {
+  id: number;
+  text: string;
+  done: boolean;
+};
+
 export const TodoApp = () => {
-  const [todos, setTodos] = useState([
+  const DEFAULT_TODOS = [
     {
       id: 1,
       text: "TDD 배우기",
@@ -13,12 +19,48 @@ export const TodoApp = () => {
       text: "react-testing-library 사용하기",
       done: true,
     },
-  ]);
-  const onToggle = () => {};
+  ];
+  const [todos, setTodos] = useState<Todo[]>(DEFAULT_TODOS);
+  const nextId = useRef(3);
+
+  // const onInsert = ({
+  //   target: { value },
+  // }: React.ChangeEvent<HTMLInputElement>) => {
+  //   const newObj = {
+  //     id: nextId.current,
+  //     text: value,
+  //     done: false,
+  //   };
+  //   setTodos([newObj]);
+  // };
+
+  const onInsert = useCallback(
+    (text: string) => {
+      // 새 항목 추가 후
+      setTodos(
+        todos.concat({
+          id: nextId.current,
+          text,
+          done: false,
+        })
+      );
+      // nextId 값에 1 더하기
+      nextId.current += 1;
+    },
+    [todos]
+  );
+
+  const onToggle = (id: number) => {
+    setTodos(() => {
+      return todos.map((todo: Todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo
+      );
+    });
+  };
   const onRemove = () => {};
   return (
     <React.Fragment>
-      <TodoForm />
+      <TodoForm onInsert={onInsert} />
       <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </React.Fragment>
   );
