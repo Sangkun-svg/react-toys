61903d11d95a3efa399adcb9ec077aec8459c761
Sangkun-svg <gims4752@gmail.com>
2022-10-25 18:12:43 +0900

    test : input onchange test

--- todo-with-tdd/src/__test__/TodoForm/TodoForm.test.tsx
+++ todo-with-tdd/src/__test__/TodoForm/TodoForm.test.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import { TodoForm } from "../../components";
 
 describe("<TodoForm />", () => {
-  it("should have @", () => {
+  it("should have Input & Button", () => {
     render(<TodoForm />);
     screen.getByText("등록");
     screen.getByPlaceholderText("할 일을 입력해주세요");
+  });
+
+  it("changes input", () => {
+    render(<TodoForm />);
+    const input = screen.getByPlaceholderText("할 일을 입력해주세요");
+    fireEvent.change(input, {
+      target: {
+        value: "TDD 배우기",
+      },
+    });
+    expect(input).toHaveAttribute("value", "TDD 배우기");
   });
 });
--- todo-with-tdd/src/components/TodoForm/TodoForm.tsx
+++ todo-with-tdd/src/components/TodoForm/TodoForm.tsx
@@ -1,7 +1,19 @@
+import React, { useState } from "react";
+
 export const TodoForm = () => {
+  const [value, setValue] = useState<string>("");
+  const onChange = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLInputElement>): void => {
+    setValue(value);
+  };
   return (
     <form>
-      <input placeholder="할 일을 입력해주세요" />
+      <input
+        placeholder="할 일을 입력해주세요"
+        value={value}
+        onChange={onChange}
+      />
       <button>등록</button>
     </form>
   );
