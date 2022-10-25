cd93302f04abd55db2e3064953c3436984bbe883
Sangkun-svg <gims4752@gmail.com>
2022-10-25 19:28:43 +0900

    test : render todos

--- todo-with-tdd/src/__test__/TodoList/TodoList.test.tsx
+++ todo-with-tdd/src/__test__/TodoList/TodoList.test.tsx
@@ -2,6 +2,23 @@ import React from "react";
 import { render, fireEvent, screen } from "@testing-library/react";
 import { TodoList } from "../../components";
 
+const sampleTodos = [
+  {
+    id: 1,
+    text: "TDD 배우기",
+    done: true,
+  },
+  {
+    id: 2,
+    text: "react-testing-library 사용하기",
+    done: true,
+  },
+];
+
 describe("<TodoList />", () => {
-  //
+  it("renders todos properly", () => {
+    render(<TodoList todos={sampleTodos} />);
+    screen.getByText(sampleTodos[0].text);
+    screen.getByText(sampleTodos[1].text);
+  });
 });
--- todo-with-tdd/src/components/TodoList/TodoList.tsx
+++ todo-with-tdd/src/components/TodoList/TodoList.tsx
@@ -1,3 +1,25 @@
-export const TodoList = () => {
-  return <ul></ul>;
+import React from "react";
+import { TodoItem } from "..";
+
+type Todo = {
+  id: number;
+  text: string;
+  done: boolean;
+};
+
+export const TodoList = ({ todos }: any | Todo[]) => {
+  return (
+    <ul>
+      {todos.map((todo: Todo) => {
+        return (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onRemove={() => {}}
+            onToggle={() => {}}
+          />
+        );
+      })}
+    </ul>
+  );
 };
