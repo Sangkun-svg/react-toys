cfeebbfa407148076462e0524dbeaf22dd6ff3f2
Sangkun-svg <gims4752@gmail.com>
2022-10-25 19:10:09 +0900

    test : about onClick function test

--- todo-with-tdd/src/components/TodoItem/TodoItem.tsx
+++ todo-with-tdd/src/components/TodoItem/TodoItem.tsx
@@ -6,19 +6,26 @@ type Props = {
     text: string;
     done: boolean;
   };
+  onToggle: (id: number) => void;
+  onRemove: (id: number) => void;
 };
 
-export const TodoItem = ({ todo: { id, text, done } }: Props) => {
+export const TodoItem = ({
+  todo: { id, text, done },
+  onToggle,
+  onRemove,
+}: Props) => {
   return (
     <li>
       <span
         style={{
           textDecoration: done ? "line-through" : "none",
         }}
+        onClick={() => onToggle(id)}
       >
         {text}
       </span>
-      <button>삭제</button>
+      <button onClick={() => onRemove(id)}>삭제</button>
     </li>
   );
 };
