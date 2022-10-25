92c1b543f4eb386a6f26c1f3db80dbeb867c2dd0
Sangkun-svg <gims4752@gmail.com>
2022-10-25 19:13:41 +0900

    feat: generate todo list component & test file

---
+++ todo-with-tdd/src/__test__/TodoList/TodoList.test.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import { TodoList } from "../../components";
+
+describe("<TodoList />", () => {
+  //
+});
--- todo-with-tdd/src/components/index.ts
+++ todo-with-tdd/src/components/index.ts
@@ -1,2 +1,3 @@
 export { TodoForm } from "./TodoForm/TodoForm";
 export { TodoItem } from "./TodoItem/TodoItem";
+export { TodoList } from "./TodoList/TodoList";
