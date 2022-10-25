2dfe4fdefdf387ece9e7a56fcc951faafdb05332
Sangkun-svg <gims4752@gmail.com>
2022-10-25 18:31:05 +0900

    test : calls onInsert and clears input

--- todo-with-tdd/src/__test__/TodoForm/TodoForm.test.tsx
+++ todo-with-tdd/src/__test__/TodoForm/TodoForm.test.tsx
@@ -19,4 +19,20 @@ describe("<TodoForm />", () => {
     });
     expect(input).toHaveAttribute("value", "TDD 배우기");
   });
+
+  it("calls onInsert and clears input", () => {
+    const onInsert = jest.fn();
+    render(<TodoForm onInsert={onInsert} />);
+    const input = screen.getByPlaceholderText("할 일을 입력해주세요");
+    const button = screen.getByText("등록");
+    fireEvent.change(input, {
+      target: {
+        value: "TDD 배우기",
+      },
+    });
+    fireEvent.click(button);
+
+    expect(onInsert).toBeCalledWith("TDD 배우기"); // onInsert 가 'TDD 배우기' 파라미터가 호출됐어야함
+    expect(input).toHaveAttribute("value", ""); // input이 비워져야함
+  });
 });
--- todo-with-tdd/src/components/TodoForm/TodoForm.tsx
+++ todo-with-tdd/src/components/TodoForm/TodoForm.tsx
@@ -1,20 +1,32 @@
 import React, { useState } from "react";
 
-export const TodoForm = () => {
+type Props = {
+  onInsert: () => void;
+};
+
+export const TodoForm = ({ onInsert }: any) => {
   const [value, setValue] = useState<string>("");
+
   const onChange = ({
     target: { value },
   }: React.ChangeEvent<HTMLInputElement>): void => {
     setValue(value);
   };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    onInsert(value);
+    setValue("");
+    event.preventDefault();
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <input
         placeholder="할 일을 입력해주세요"
         value={value}
         onChange={onChange}
       />
-      <button>등록</button>
+      <button type="submit">등록</button>
     </form>
   );
 };
