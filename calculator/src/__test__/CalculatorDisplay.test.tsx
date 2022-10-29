ad3d2ec55510bcf15964e3444bcbf74b72eeb5b0
Sangkun-svg <gims4752@gmail.com>
2022-10-29 17:45:23 +0900

    feat : input value change

--- calculator/src/__test__/CalculatorDisplay.test.tsx
+++ calculator/src/__test__/CalculatorDisplay.test.tsx
@@ -1,10 +1,19 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { CalculatorDisplay } from "../components";
 
-describe.only("<CalculatorDisplay />", () => {
+describe("<CalculatorDisplay />", () => {
   it("should have default value === 0", () => {
     render(<CalculatorDisplay />);
-    const defaultValue = screen.getByDisplayValue(0);
+    const defaultValue = screen.getByPlaceholderText("0");
     expect(defaultValue).toBeTruthy();
+  });
+
+  it("should change input", () => {
+    render(<CalculatorDisplay />);
+    const input = screen.getByPlaceholderText("0");
+    fireEvent.change(input, { target: { value: 100 } });
+    console.log(input);
+
+    expect(screen.getByDisplayValue(100)).toBeTruthy();
   });
 });
--- calculator/src/components/CalculatorDisplay.tsx
+++ calculator/src/components/CalculatorDisplay.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const CalculatorDisplay = () => {
+  const [enteredValue, setEnteredValue] = useState<number | string>(0);
+
+  const onChangeEnteredValue = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLInputElement>): void => {
+    setEnteredValue(value);
+  };
+
   return (
     <div>
-      <input defaultValue={0} />
+      <input
+        value={enteredValue}
+        onChange={onChangeEnteredValue}
+        placeholder="0"
+      />
     </div>
   );
 };
