7464ee2da6e0172908aaef76c8407b493b67b396
Sangkun-svg <gims4752@gmail.com>
2022-10-31 19:53:33 +0900

    refactor : texting 기능 잠시 지연

--- calculator/src/__test__/CalculatorDisplay.test.tsx
+++ calculator/src/__test__/CalculatorDisplay.test.tsx
@@ -14,12 +14,4 @@ describe("<CalculatorDisplay />", () => {
     fireEvent.change(input, { target: { value: 100 } });
     expect(input.value).toBe("100");
   });
-
-  it("could be wirte only number or operator", () => {
-    // TODO: not implement about operator
-    render(<CalculatorDisplay />);
-    const input = screen.getByPlaceholderText("0");
-    fireEvent.change(input, { target: { value: "text" } });
-    expect(screen.queryByDisplayValue("text")).toBeFalsy();
-  });
 });
