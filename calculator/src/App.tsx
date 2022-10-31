0c99bac800ccdfc81c8ec96b4fea29ac9ec41c16
Sangkun-svg <gims4752@gmail.com>
2022-10-31 19:23:13 +0900

    design : delete unused tag

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -2,9 +2,5 @@ import React from "react";
 import { CalculratorApp } from "./components";
 
 export default function App() {
-  return (
-    <div>
-      <CalculratorApp />
-    </div>
-  );
+  return <CalculratorApp />;
 }
