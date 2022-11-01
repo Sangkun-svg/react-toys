5399530757217b2ec371a6f13ca162abc42d0918
Sangkun-svg <gims4752@gmail.com>
2022-11-01 18:13:13 +0900

    test : add exist  operator button

--- calculator/src/__test__/CalculatorBtn.test.tsx
+++ calculator/src/__test__/CalculatorBtn.test.tsx
@@ -1,10 +1,28 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { CalculatorBtn } from "../components";
 
 describe("<CalculatorBtn />", () => {
   it("should has number Button", () => {
     render(<CalculatorBtn />);
-    const btns = screen.getAllByText(/^[0-9]/);
-    expect(btns.length).toBe(10);
+    const NUMBER_buttons = screen.getAllByText(/^[0-9]/);
+    expect(NUMBER_buttons).toHaveLength(10);
+  });
+  it("should has operator Button", () => {
+    render(<CalculatorBtn />);
+    const AC_button = screen.getByText("AC");
+    const DEL_button = screen.getByText("DEL");
+    const PERSENT_button = screen.getByText("%");
+    const PLUS_button = screen.getByText("+");
+    const MINUS_button = screen.getByText("-");
+    const MULTI_button = screen.getByText("x");
+    const DIVISION_button = screen.getByText("÷");
+
+    expect(AC_button).toBeInTheDocument();
+    expect(DIVISION_button).toBeInTheDocument();
+    expect(MULTI_button).toBeInTheDocument();
+    expect(MINUS_button).toBeInTheDocument();
+    expect(PLUS_button).toBeInTheDocument();
+    expect(PERSENT_button).toBeInTheDocument();
+    expect(DEL_button).toBeInTheDocument();
   });
 });
