dc322212c6799c4fc755e6d7dff8bcc9ac170122
Sangkun-svg <gims4752@gmail.com>
2022-11-01 18:35:47 +0900

    test : apply setup function

--- calculator/src/__test__/CalculratorApp.test.tsx
+++ calculator/src/__test__/CalculratorApp.test.tsx
@@ -13,38 +13,33 @@ describe("<CalculatorDisplay />", () => {
   };
 
   it("input value all clear when click AC button", () => {
-    render(<CalculratorApp />);
-    const AC_button: HTMLButtonElement = screen.getByText("AC");
-    const input: HTMLInputElement = screen.getByPlaceholderText("0");
+    const { input, generateOperBtn } = setup();
+    const AC_button = generateOperBtn("AC");
     fireEvent.change(input, { target: { value: 1000 } });
     fireEvent.click(AC_button);
     expect(input.value).toBe("");
   });
 
   it("delete last index in input value", () => {
-    render(<CalculratorApp />);
-    const DEL_button: HTMLButtonElement = screen.getByText("DEL");
-    const input: HTMLInputElement = screen.getByPlaceholderText("0");
+    const { input, generateOperBtn } = setup();
+    const DEL_button = generateOperBtn("DEL");
     fireEvent.change(input, { target: { value: 1000 } });
     fireEvent.click(DEL_button);
     expect(input.value).toBe("100");
   });
 
   it("input value then click number button", () => {
-    render(<CalculratorApp />);
-    const input: HTMLInputElement = screen.getByPlaceholderText("0");
-    const NUMBER_button: HTMLButtonElement = screen.getByText("1");
+    const { input, generateOperBtn } = setup();
+    const NUMBER_button = generateOperBtn("1");
     fireEvent.click(NUMBER_button);
-    fireEvent.click(NUMBER_button);
-    expect(input.value).toBe("11");
+    expect(input.value).toBe("1");
   });
 
   it("input value then click operator plus button", () => {
-    render(<CalculratorApp />);
-    const input: HTMLInputElement = screen.getByPlaceholderText("0");
-    const NUMBER_button: HTMLButtonElement = screen.getByText("1");
-    const OPER_button: HTMLButtonElement = screen.getByText("+");
-    const SUM_button: HTMLButtonElement = screen.getByText("=");
+    const { input, generateOperBtn } = setup();
+    const NUMBER_button = generateOperBtn("1");
+    const OPER_button = generateOperBtn("+");
+    const SUM_button = generateOperBtn("=");
     fireEvent.click(NUMBER_button);
     fireEvent.click(OPER_button);
     fireEvent.click(NUMBER_button);
@@ -53,12 +48,11 @@ describe("<CalculatorDisplay />", () => {
   });
 
   it("input value then click operator minus button", () => {
-    render(<CalculratorApp />);
-    const input: HTMLInputElement = screen.getByPlaceholderText("0");
-    const NUMBER_Two_button: HTMLButtonElement = screen.getByText("2");
-    const NUMBER_One_button: HTMLButtonElement = screen.getByText("1");
-    const OPER_button: HTMLButtonElement = screen.getByText("-");
-    const SUM_button: HTMLButtonElement = screen.getByText("=");
+    const { input, generateOperBtn } = setup();
+    const NUMBER_Two_button = generateOperBtn("2");
+    const NUMBER_One_button = generateOperBtn("1");
+    const OPER_button = generateOperBtn("-");
+    const SUM_button = generateOperBtn("=");
     fireEvent.click(NUMBER_Two_button);
     fireEvent.click(OPER_button);
     fireEvent.click(NUMBER_One_button);
@@ -67,11 +61,10 @@ describe("<CalculatorDisplay />", () => {
   });
 
   it("input value then click operator multiplication button", () => {
-    render(<CalculratorApp />);
-    const input: HTMLInputElement = screen.getByPlaceholderText("0");
-    const NUMBER_Two_button: HTMLButtonElement = screen.getByText("2");
-    const OPER_button: HTMLButtonElement = screen.getByText("x");
-    const SUM_button: HTMLButtonElement = screen.getByText("=");
+    const { input, generateOperBtn } = setup();
+    const NUMBER_Two_button = generateOperBtn("2");
+    const OPER_button = generateOperBtn("x");
+    const SUM_button = generateOperBtn("=");
     fireEvent.click(NUMBER_Two_button);
     fireEvent.click(OPER_button);
     fireEvent.click(NUMBER_Two_button);
@@ -80,12 +73,11 @@ describe("<CalculatorDisplay />", () => {
   });
 
   it("input value then click operator division button", () => {
-    render(<CalculratorApp />);
-    const input: HTMLInputElement = screen.getByPlaceholderText("0");
-    const NUMBER_Two_button: HTMLButtonElement = screen.getByText("2");
-    const NUMBER_Four_button: HTMLButtonElement = screen.getByText("4");
-    const OPER_button: HTMLButtonElement = screen.getByText("÷");
-    const SUM_button: HTMLButtonElement = screen.getByText("=");
+    const { input, generateOperBtn } = setup();
+    const NUMBER_Two_button = generateOperBtn("2");
+    const NUMBER_Four_button = generateOperBtn("4");
+    const OPER_button = generateOperBtn("÷");
+    const SUM_button = generateOperBtn("=");
     fireEvent.click(NUMBER_Four_button);
     fireEvent.click(OPER_button);
     fireEvent.click(NUMBER_Two_button);
@@ -94,18 +86,16 @@ describe("<CalculatorDisplay />", () => {
   });
 
   it("if click operator button when input is empty", () => {
-    render(<CalculratorApp />);
-    const input: HTMLInputElement = screen.getByPlaceholderText("0");
-    const DOT_button: HTMLButtonElement = screen.getByText(".");
+    const { input, generateOperBtn } = setup();
+    const DOT_button = generateOperBtn(".");
     fireEvent.click(DOT_button);
     expect(input.value).toBe("");
   });
 
   it("if click operator button when input prev value is Operator", () => {
-    render(<CalculratorApp />);
-    const input: HTMLInputElement = screen.getByPlaceholderText("0");
-    const NUMBER_Two_button: HTMLButtonElement = screen.getByText("2");
-    const Plus_button: HTMLButtonElement = screen.getByText("+");
+    const { input, generateOperBtn } = setup();
+    const NUMBER_Two_button = generateOperBtn("2");
+    const Plus_button = generateOperBtn("+");
     fireEvent.click(NUMBER_Two_button);
     fireEvent.click(Plus_button);
     fireEvent.click(Plus_button);
